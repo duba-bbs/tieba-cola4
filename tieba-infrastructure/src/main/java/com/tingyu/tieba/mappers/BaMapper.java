dc7dfa0fbb34face4936b4e2f6d916929e3d5f5d
luo0412 <1095847440@qq.com>
2021-04-01 17:14:59 +0800

    [](fix)mapper param err

--- tieba-infrastructure/src/main/java/com/tingyu/tieba/mappers/BaMapper.java
+++ tieba-infrastructure/src/main/java/com/tingyu/tieba/mappers/BaMapper.java
@@ -3,20 +3,19 @@ package com.tingyu.tieba.mappers;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
-
 import com.tingyu.tieba.ba.dataobject.BaDO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
-public interface BaMapper {
+public interface BaMapper{
 
-    public Long create(BaDO baDO);
+  public Long create(BaDO baDO);
 
-    public BigDecimal update(BaDO baDO);
+  public BigDecimal update(BaDO baDO);
 
-    public BigDecimal delete(@Param("id") BigInteger id);
-
-    public List<BaDO> list(BaDO baDO);
+  public BigDecimal delete(@Param("id") BigInteger id);
+  
+  public List<BaDO> list(BaDO baDO);
 
 }
